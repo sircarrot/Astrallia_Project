5a7819d8adcc1e13e8e4f8ed2c73650c039d7a3c
Royce Santo <roycesanto@yahoo.com>
2019-04-09 20:41:56 +0800

    Imported my own package, added camera and player controls, exported fonts

---
+++ Astrallia Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AstralliaProject
+{
+    public class CameraController : MonoBehaviour
+    {
+        void Start()
+        {
+            
+
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            float horizontalAxis = Input.GetAxis("Mouse X");
+            float verticalAxis = Input.GetAxis("Mouse Y");
+
+
+        }
+    }
+}
---
+++ Astrallia Project/Assets/_CarrotPack/Scripts/Audio/AudioLibrary.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+
+
+namespace CarrotPack
+{
+
+    [CreateAssetMenu(menuName = "Royce/AudioLibrary", fileName = "AudioLibrary")]
+    public class AudioLibrary : SerializedScriptableObject
+    {
+        [Header("Audio Library")]
+        [DictionaryDrawerSettings(DisplayMode = DictionaryDisplayOptions.OneLine,
+                IsReadOnly = false,
+                KeyLabel = "Name",
+                ValueLabel = "AudioClip")]
+        [GUIColor(0.7f, 0.7f, 1)]
+        public Dictionary<string, AudioClip> audioLibrary = new Dictionary<string, AudioClip>();
+    
+        public void InitLibrary()
+        {
+
+        }
+    }
+}
+
+#else
+
+namespace CarrotPack
+{
+    [CreateAssetMenu(menuName = "Royce/AudioLibrary", fileName = "AudioLibrary")]
+    public class AudioLibrary : ScriptableObject
+    {
+        [Header("Audio Library")]
+
+        public List<AudioClip> audioList = new List<AudioClip>();
+
+        [HideInInspector] public Dictionary<string, AudioClip> audioLibrary = new Dictionary<string, AudioClip>();
+
+        public void InitLibrary()
+        {
+            foreach(AudioClip audioClip in audioList)
+            {
+                audioLibrary.Add(audioClip.name, audioClip);
+            }
+        }
+    }
+}
+
+
+
+#endif
+
