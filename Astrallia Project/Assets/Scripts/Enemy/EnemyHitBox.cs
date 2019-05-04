c154f37fb113d6104b708a5c3bb7d77233766775
Royce Santo <roycesanto@yahoo.com>
2019-05-04 23:56:59 +0800

    Enemy HitBox

---
+++ Astrallia Project/Assets/Scripts/Enemy/EnemyHitBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AstralliaProject
+{
+    public class EnemyHitBox : MonoBehaviour
+    {
+        public Enemy enemy;
+
+        private void Start()
+        {
+            if (enemy == null) enemy = GetComponentInParent<Enemy>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            //Debug.Log(other.name);
+            enemy.AttackCollision(other);
+        }
+
+    }
+}
