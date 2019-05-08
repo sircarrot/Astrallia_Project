52e77bb1f6f683ca8ed01ef6dd44a045c266daaf
Royce Santo <roycesanto@yahoo.com>
2019-05-08 23:56:14 +0800

    Start Scene

---
+++ Astrallia Project/Assets/Scripts/UI/UIAnimatorEvents.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CarrotPack;
+
+namespace AstralliaProject
+{
+    public class UIAnimatorEvents : MonoBehaviour
+    {
+        public void ActionCallback()
+        {
+            Toolbox.Instance.GetManager<UIManager>().ActionCallback();
+        }
+    }
+}
